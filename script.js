79048401e8e25ec82f6546734ba20dba75c9ba8f
Emanuelle Viana <manuavbr@gmail.com>
2022-04-25 21:05:21 -0300

    update input error e accept

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ function logar() {
     let senha = document.getElementById('senha')
     let res = document.getElementById('res')
     let arroba = document.getElementById('arroba')
+    let lock = document.getElementById('lock')
 
     res.innerHTML=""
     if (nome.value == null || senha.value == null || nome.value == "" || senha.value == "") {
         nome.style.borderColor="#FF5757";
-        res.innerHTML="Por favor, insira um email válido!"
+        res.innerHTML="Por favor, preencha os campos corretamente!"
         nome.style.placeholder="example@gmail.com"
-        arroba.style.color="red"
-
+        arroba.style.color="red";
+        senha.style.borderColor="#FF5757";
+        arroba.style.color="#FF5757";
+        lock.style.color="#FF5757";
     } else {
         nome.style.borderColor="green";
         senha.style.borderColor="green";
-
+        arroba.style.color="green";
+        lock.style.color="green";
     }
 
 }
